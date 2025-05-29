044483fa359e4c1d8bb198fc40b375b88cac5502
Rajesh-K-C <rajeshkc1111.rk@gmail.com>
2025-05-29 17:57:18 +0545

    feat: add basic PyQt6 window setup

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from text2speech-app!")
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Text To Speech App")
+        self.resize(600,300)
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
 
 if __name__ == "__main__":
     main()
